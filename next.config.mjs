0505ddd84041180fad52560572c4c810702ba55a
Owen Rummage <owen@rummage.cc>
2024-11-22 11:37:47 -0600

    update deployment config

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,9 @@ const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
-  output: "export"
+  output: "export",
+  
+  distDir: 'dist'
 }
  
 const withMDX = createMDX({
