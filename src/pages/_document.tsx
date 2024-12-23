3ae09a21f9feb3dd405a92b409b536fbafedc09f
Owen Rummage <owen@rummage.cc>
2024-12-23 07:59:42 -0600

    fix sideways scroll issue; add favicon to be correct

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,11 @@ import Footer from "@/components/Footer";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/logo.svg" sizes="any" />
+      </Head>
       <body
-        className={`bg-gradient-to-br from-[#050505] to-[#0f0f0f] antialiased w-screen min-h-screen flex flex-col items-center p-4 transition`}
+        className={`bg-gradient-to-br from-[#050505] to-[#0f0f0f] antialiased w-screen overflow-x-hidden min-h-screen flex flex-col items-center p-4 transition`}
       >
         <NavBar />
         <div className="md:w-4/6 md:pt-24">
